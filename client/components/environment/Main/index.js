58136f8728bacf618f1ae427d19c3610cc8cf9cd
Almog Adziashvili <almoga@wix.com>
2024-08-13 14:54:53 +0300

    remove file

--- client/components/environment/Main/index.js
+++ client/components/environment/Main/index.js
@@ -1,3 +1,23 @@
-import Main from './Main';
+import React from 'react';
 
-export default Main;
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import HomePage from '../../../components/pages/HomePage';
+
+import styles from './styles.module.css';
+
+export default function Main() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
+
+  return (
+    <div className={styles.main}>
+      <RouterProvider router={router} />
+    </div >
+  );
+}
--- client/components/pages/HomePage/index.js
+++ client/components/pages/HomePage/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-
     <div>
-      hi
+      HomePage
     </div>
   );
 }
